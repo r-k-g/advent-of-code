97d9979d8434f2a0acf094a69bff5cd2cd26e72d
Rowan <dragonologist.ro@gmail.com>
2021-12-05 21:23:33 -0800

    2021 day 6

---
+++ events/2021/day6.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+from lib import *
+problem = aoc.Problem("2021/06: Lanternfish")
+problem.preprocessor = ppr.csi
+
+@problem.solver()
+def solve(fishes):
+    days = 256
+
+    fishes = Counter(fishes)
+
+    for d in range(days):
+        school = fishes.copy()
+
+        for time in school:
+            if time == 0:
+                fishes[8] += school[time]
+                fishes[0] -= school[time]
+                fishes[6] += school[time]
+            else:
+                fishes[time] -= school[time]
+                fishes[time-1] += school[time]
+
+        if d == 79:
+            count1 = sum(fishes.values())
+
+    count2 = sum(fishes.values())
+
+    return (count1, count2)
+
+SAMPLE_INP =\
+"""3,4,3,1,2
+"""
+
+if __name__ == "__main__":
+    problem.solve(SAMPLE_INP, 5934, 26984457539)
