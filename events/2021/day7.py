d53a01a38b7e3b67bbabf3e1c1f8c391f9aeaa51
Rowan <dragonologist.ro@gmail.com>
2021-12-06 22:26:58 -0800

    2021 day 7

---
+++ events/2021/day7.py
@@ -0,0 +1,26 @@
+from lib import *
+problem = aoc.Problem("2021/07: The Treachery of Whales")
+problem.preprocessor = ppr.csi
+
+@problem.solver()
+def solve(inp):
+    positions = []
+
+    for i in range(min(inp), max(inp) + 1):
+        fuel1 = 0
+        fuel2 = 0
+        for x in inp:
+            dist = abs(i-x)
+            fuel1 += dist
+            fuel2 += dist * (dist + 1) // 2
+        positions.append(fuel1)
+        positions.append(fuel2)
+    
+    return (min(positions[::2]), min(positions[1::2]))
+
+SAMPLE_INP =\
+"""16,1,2,0,4,2,7,1,2,14
+"""
+
+if __name__ == "__main__":
+    problem.solve(SAMPLE_INP, 37, 168)
