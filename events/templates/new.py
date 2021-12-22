968284fbab6950bdeb403a053126c3d47c416dfd
Rowan <dragonologist.ro@gmail.com>
2021-12-21 19:49:39 -0800

    all symlinks broken??

---
+++ events/templates/new.py
@@ -0,0 +1,14 @@
+import aoc
+
+helper = aoc.Helper("2021/01", "Sonar Sweep")
+
+SAMPLE = """
+"""
+helper.check_sample(SAMPLE, 0, 0)
+
+inp = helper.get_input().strip()
+
+
+print("Part 1:", 0)
+
+# print("Part 2:", 0)
